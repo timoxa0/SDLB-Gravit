9f21cb545df6fa50d2931a7c3f41ec639ab5dfca
Timofey <tima012007@gmail.com>
2022-05-07 18:44:56 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt update && apt -y install php-common libapache2-mod-php php-cli php-gd python3 python3-pip git mariadb-server nano screen curl && curl -o /tmp/GravitDSBot.py https://raw.githubusercontent.com/timoxa0/discord-auth/main/install.py && python3 /tmp/GravitDSBot.py
+apt update && apt -y install php-common libapache2-mod-php libapache2-mod-security2 php-cli php-gd python3 python3-pip git mariadb-server nano screen curl && curl -o /tmp/GravitDSBot.py https://raw.githubusercontent.com/timoxa0/discord-auth/main/install.py && python3 /tmp/GravitDSBot.py
