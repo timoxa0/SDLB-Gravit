2037bebde93c56313d88d98f7f1b6f92b7ae6ec0
timoxa0 <tima012007@gmail.com>
2022-06-20 19:15:20 +0500

    Отказ от TextureProvider в пользу самописа

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 apt update
 apt install -y python3 python3-pip git mariadb-server nano screen curl
-curl -o /tmp/GravitDSBot.py https://raw.githubusercontent.com/timoxa0/SDLB-Gravit/main/install.py
-python3 /tmp/GravitDSBot.py
+git clone -b dev https://github.com/timoxa0/SDLB-Gravit
